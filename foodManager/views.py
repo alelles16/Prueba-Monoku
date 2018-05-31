fe4994ee3d839b45728740433a0a349e5500a26a
Ana Elles <alelles16@gmail.com>
2018-05-30 21:51:34 -0500

    Implements Templates

--- foodManager/views.py
+++ foodManager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def productos_list (request):
-    return render(request, 'foodManager/productos_list.html', {})
+    productos = Producto.objects.order_by('nombre')
+    return render(request, 'foodManager/productos_list.html', {'productos': productos})
